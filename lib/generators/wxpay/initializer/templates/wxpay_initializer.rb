afddf8fe0d86aaa6f1cb27bd96d39aca28407ae0
hzh <sforce1000@gmail.com>
2014-08-26 22:07:03 +0800

    example config

--- lib/generators/wxpay/initializer/templates/wxpay_initializer.rb
+++ lib/generators/wxpay/initializer/templates/wxpay_initializer.rb
@@ -4,4 +4,13 @@ Wxpay.configure do |config|
   # config.pay_sign_key = conf_wxpay.pay_sign_key
   # config.partner_id = conf_wxpay.partner_id
   # config.partner_key = conf_wxpay.partner_key
+
+  # config.is_multi = true
+  # config.custom_config = Proc.new do |config|
+  #   # config.app_id = conf_wxpay.app_id
+  #   # config.app_secret = conf_wxpay.app_secret
+  #   # config.pay_sign_key = conf_wxpay.pay_sign_key
+  #   # config.partner_id = conf_wxpay.partner_id
+  #   # config.partner_key = conf_wxpay.partner_key    
+  # end
 end
